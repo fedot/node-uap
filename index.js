af4ec8c4acd67ef227c3dd8e8f6bbe2391dd4385
Alex <fastex93@gmail.com>
2016-07-13 14:13:33 +0500

    added array.find ponyfill

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var path = require('path'),
     fs = require('fs'),
     yaml = require('yamlparser'),
+    find = require('array-find'),
     regexes_base_path = 'node_modules/uap-core/regexes.yaml',
-    uap_regexes_path = [
+    uap_regexes_path = find([
         path.join(__dirname, regexes_base_path),
         path.join(__dirname, '../..', regexes_base_path)
-    ].find(fs.existsSync),
+    ], fs.existsSync),
     refImpl = require('uap-ref-impl')(readYAML(uap_regexes_path));
 
 function readYAML(file) {
